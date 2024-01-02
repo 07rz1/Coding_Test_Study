0591929b6124c091f608469f5b014a91c21886fb
용가리 <phoenixgiha@gmail.com>
2024-01-03 01:07:08 +0900

    Ａ＋Ｂ / Bronze V / ２분

--- BAEKJOON/Bronze/1000.A+B/Main.java
+++ BAEKJOON/Bronze/1000.A+B/Main.java
@@ -2,17 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+	int A, B;
 	Scanner sc = new Scanner(System.in);
-	System.out.println("A와 B 값을 입력하세요. 값은 공백으로 구분해주세요.");
-	int A = sc.nextInt();
-	int B = sc.nextInt();
-	if (A <= 0) {
-		System.out.println("Error: A는 0보다 커야합니다.");
-		} else if (B >= 10) {
-		System.out.println("Error: B는 10보다 작아야합니다.");
-		} else {
-		int answer = A+B;
-		System.out.println(answer);
-		}
+	A = sc.nextInt();
+	B = sc.nextInt();
+	int answer = A+B;
+	System.out.println(answer);
 	}
 }
