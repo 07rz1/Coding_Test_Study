401538e1e7a56eae87d7b47dd55c0bba98634a45
용가리 <phoenixgiha@gmail.com>
2024-01-02 23:07:29 +0900

    Update Solution.java
    문자열의 단어 개수가 문제에서 요구한 인수와 같은 점을 활용

--- programmers/Lv.0/PCCE_pastquestions_no1/Solution.java
+++ programmers/Lv.0/PCCE_pastquestions_no1/Solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public static void main(String[] args) {
         String msg = "Spring is beginning";
-        int val1 = 3;
-        String val2 = String.valueOf(3);
+        int val1 = msg.split(" ").length; //공백 기준 단어 개수
+        String val2 = String.valueOf(val1);
 
         System.out.println(msg);
         System.out.println(val1 + 10);
