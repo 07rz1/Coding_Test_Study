db2949ecbfde7324b4e24e7f35788017bf67ab10
용가리 <phoenixgiha@gmail.com>
2024-01-19 00:58:00 +0900

    수 조작하기1 / Lv.0 / 15분

--- programmers/Lv.0/Day6_manipulatenumbers/Solution.java
+++ programmers/Lv.0/Day6_manipulatenumbers/Solution.java
@@ -1,11 +1,4 @@
 public class Solution {
-    public static void main(String[] args) {
-        int n = 5;
-        String control = "wasd";
-        int result = solution(n, control);
-        System.out.println(result);
-    }
-
     public static int solution(int n, String control) {
         for (char ch : control.toCharArray()) {
             switch (ch) {
